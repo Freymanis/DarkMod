02c447862d7bf0a6a272df6d19b6dc24ea22c124
Freymanis <nfreymanis@gmail.com>
2022-02-08 23:37:09 +0300

    Initial Commit

---
+++ DarkMod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DarkMod
+//
+//  Created by Nikita Freymanis on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
